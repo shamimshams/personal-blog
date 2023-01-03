94b5fd7e9f5085fb92fb8af21df8de285e972b07
shamim shams <shamim.idb@hotmail.com>
2023-01-03 22:43:20 +0600

    force https added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Filament\Facades\Filament;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Filament::serving(function () {
             // Using Vite
             Filament::registerViteTheme('resources/css/app.css');
